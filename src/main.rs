fc4e546037014c94a2a07fa564fda9810211997d
unixtensor <brandon@rhpidfyre.io>
2024-11-21 15:38:14 -0500

    cli init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+#[path = "./shell/shell.rs"]
+mod shell;
+#[path = "./shell/ps.rs"]
+mod ps;
+#[path = "./shell/commands.rs"]
+mod commands;
+#[path = "./shell/rc.rs"]
+mod rc;
+#[path = "./shell/cli.rs"]
+mod cli;
+#[path = "./shell/verbose.rs"]
+mod verbose;
+
+fn main() {
+	if let Some(args) = cli::parser() {
+		let shell_config = shell::Config {
+			norc: args.norc,
+			verbose: args.verbose,
+			extra_color: args.extra_color
+		};
+		let mut shell_instance = shell::LambdaShell::create(shell_config);
+		shell_instance.start();
+	};
+}
---
+++ src/shell/cli.rs
@@ -0,0 +1,44 @@
+use clap::Parser;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Parser)]
+#[command(about, long_about = None)]
+pub struct Cli {
+	#[arg(short, long)]
+	pub version: bool,
+	///Extra output mode, primarily used for debugging
+	#[arg(long)]
+	pub verbose: bool,
+	///Built-in shell commands and shell output will be more colorful (Will not work for non-ANSI terminals)
+	#[arg(long)]
+	pub extra_color: bool,
+	///Download and install plugin(s)
+	#[arg(long)]
+	pub add_plugins: Vec<String>,
+	//Remove plugin(s)
+	#[arg(long)]
+	pub remove_plugins: Vec<String>,
+	///Start the shell with no rc configurations
+	#[arg(long)]
+	pub norc: bool,
+	///Disable the Luau JIT backend
+	#[arg(long)]
+	pub nojit: bool,
+}
+
+pub fn version_print(cli_parser: Cli) {
+	match cli_parser.extra_color {
+	    true => color_print::cprintln!("<cyan,bold>Lambda Shell</>, version <bold>{}</>.", VERSION),
+	    false => println!("Lambda Shell, version {}.", VERSION),
+	};
+}
+
+pub fn parser() -> Option<Cli> {
+	let cli_parser = Cli::parse();
+	if cli_parser.version {
+		version_print(cli_parser);
+		return None //stop here
+	}
+	Some(cli_parser)
+}
