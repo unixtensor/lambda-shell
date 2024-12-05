af24acc508713d625d33d17378962d8dc130c6d0
rhpidfyre <brandon@rhpidfyre.io>
2024-12-05 02:48:21 -0500

    luau/types

--- src/cli.rs
+++ src/cli.rs
@@ -10,6 +10,9 @@ pub struct Cli {
 	///Extra output mode, primarily used for debugging
 	#[arg(long)]
 	pub verbose: bool,
+	///Built-in shell commands and shell output will be more colorful (Will not work for non-ANSI terminals)
+	#[arg(long)]
+	pub extra_color: bool,
 	///Download and install plugin(s)
 	#[arg(long)]
 	pub add_plugins: Vec<String>,
@@ -24,11 +27,22 @@ pub struct Cli {
 	pub nojit: bool,
 }
 
+pub fn version_print(cli_parser: Cli) {
+	match cli_parser.extra_color {
+	    true => color_print::cprintln!("<cyan,bold>Lambda Shell</>, version <bold>{}</>.", VERSION),
+	    false => println!("Lambda Shell, version {}.", VERSION),
+	};
+}
+
 pub fn parser() -> Option<Cli> {
 	let cli_parser = Cli::parse();
 	if cli_parser.version {
+<<<<<<<< HEAD:src/cli.rs
 		println!("Lambda Shell, version: {}.", VERSION);
 		println!("liblambdashell, version: {}.", liblambdashell::VERSION);
+========
+		version_print(cli_parser);
+>>>>>>>> master:src/shell/cli.rs
 		return None //stop here
 	}
 	Some(cli_parser)
